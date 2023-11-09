3d5360af62ce7220a49a1ece23bb3137a0ae0c7c
Artur Semenas <uncelfuka@gmail.com>
2023-11-09 22:57:08 +0100

    Module #4.1. Task #4. Ternary Conditions

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryGreatestNumberPrinter.java
@@ -2,5 +2,7 @@ package school.mjc.stage0.conditions.task4;
 
 public class TernaryGreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+        int a =  first < second ? second : first;
+        System.out.println(a);
     }
 }
